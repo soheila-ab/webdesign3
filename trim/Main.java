86e06461502f0aa5ca5ae6dbea60a3a22b07939e
soheila-ab <abdolahpoor.1025@gmail.com>
2021-02-18 16:31:19 +0330

    Trim exercise added

---
+++ trim/Main.java
@@ -0,0 +1,7 @@
+public class  Main {
+  public static void main(String[] args) {
+    String myStr = "       Hello World!        ";
+    System.out.println(myStr);
+    System.out.println(myStr.trim());
+  }
+}
