b5b6c8010bc5be52e91c27a01c1ea13d9d95f22f
soheila-ab <abdolahpoor.1025@gmail.com>
2021-03-04 14:00:37 +0330

    JavaScript Array map() Method

---
+++ map/p-1/index.js
@@ -0,0 +1,15 @@
+var numbers = [4, 9, 16, 25];
+
+function myFunction(){
+
+  x = document.getElementById("demo")
+
+  x.innerHTML = numbers.map(Math.sqrt);
+
+}
+
+
+
+
+
+
---
+++ map/p-2/index.js
@@ -0,0 +1,9 @@
+var numbers =  [65, 44, 12, 4];
+
+var newarray = numbers.map(myFunction)
+
+function myFunction(num) {
+    return num * 10;
+  }
+  
+  document.getElementById("demo").innerHTML = newarray;
